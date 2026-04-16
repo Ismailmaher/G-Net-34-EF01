b6be4397e180ab528a18e8fd9195eccb34bada01
Isamilmaher <ismailmaher6977@gmail.com>
2026-04-16 04:06:53 +0200

    first commit

--- G-Net-34-EF01/BookStoreContext.cs
+++ G-Net-34-EF01/BookStoreContext.cs
@@ -9,6 +9,10 @@ namespace G_Net_34_EF01
 {
     internal class BookStoreContext:DbContext
     {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=.;Database=BookStore;Trusted_Connection=True;TrustServerCertificate=true;");
